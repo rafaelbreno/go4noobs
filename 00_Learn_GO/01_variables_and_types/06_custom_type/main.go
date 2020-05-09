57953c0c59376427d513f8f4fb2ff473cee6ae18
Rafael <rafael_breno@outlook.com>
2020-05-08 23:12:33 -0300

    Added Custom Type

--- 00_Learn_GO/01_variables_and_types/06_custom_type/main.go
+++ 00_Learn_GO/01_variables_and_types/06_custom_type/main.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
-func main() {
+// type myType subType
+type myType int
 
+var b myType
+
+func main() {
+	a := 10
+	fmt.Printf("%T", a)
+	fmt.Printf("\n%T", b)
+	/**
+	* a = b this is wrong
+	* because 'a' is type 'int'
+	* and b is type 'myType'
+	**/
 }
