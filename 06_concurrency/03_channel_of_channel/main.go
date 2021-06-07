dbc546dc4ec1c48cedf111cbe08d8e2ab2227032
Rafael Breno <rafael_breno@outlook.com>
2021-06-07 20:15:27 -0300

    feat: Add channel of channel code example

--- 06_concurrency/03_channel_of_channel/main.go
+++ 06_concurrency/03_channel_of_channel/main.go
@@ -2,8 +2,67 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func main() {
+type Request struct {
+	Method   chan string
+	Endpoint string
+}
 
+func main() {
+	var wg sync.WaitGroup
+
+	methodChan := make(chan string, 4)
+
+	reqs := []Request{
+		{
+			Method:   methodChan,
+			Endpoint: "/login",
+		},
+		{
+			Method:   methodChan,
+			Endpoint: "/logout",
+		},
+		{
+			Method:   methodChan,
+			Endpoint: "/profile",
+		},
+		{
+			Method:   methodChan,
+			Endpoint: "/home",
+		},
+	}
+
+	reqChan := make(chan Request, 4)
+
+	wg.Add(1)
+
+	defReq := func(req string, ch chan string, wg *sync.WaitGroup) {
+		ch <- req
+		wg.Add(1)
+	}
+
+	go func() {
+		for _, req := range reqs {
+			reqChan <- req
+			if req.Endpoint == "/login" || req.Endpoint == "/logout" {
+				defReq("POST", methodChan, &wg)
+			} else {
+				defReq("GET", methodChan, &wg)
+			}
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		for range reqs {
+			req := <-reqChan
+			method := <-req.Method
+			fmt.Printf("%s - %s\n", req.Endpoint, method)
+			wg.Done()
+		}
+	}()
+
+	wg.Wait()
 }
